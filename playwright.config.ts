c82c0fd1181b3f512e1fb2cf4ff9af32dca84c2d
Carlos Palacios <98488075+cpalacios2@users.noreply.github.com>
2024-11-26 16:34:04 -0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [ ['html', { open: 'never' }] ],
+  reporter: [ ['html', { open: 'never', outputFolder: 'my-report' }] ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   expect:{
     timeout: 20000
