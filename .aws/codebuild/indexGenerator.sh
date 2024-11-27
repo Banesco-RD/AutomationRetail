c01cd4f296081677b71181b161eb677cf8b1f3e9
Carlos Palacios <98488075+cpalacios2@users.noreply.github.com>
2024-11-27 18:27:44 -0400

    Update indexGenerator.sh

--- .aws/codebuild/indexGenerator.sh
+++ .aws/codebuild/indexGenerator.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+BUCKET=$1
 cat .aws/codebuild/index_head.txt > index.html
 #for folder in $(aws s3 ls s3://mercury.banesco.do/|grep my-report|awk '{print $2}'|awk -F "/" '{print $1}');do
-for folder in $(aws s3 ls s3://mercury.banesco.do/ --recursive|grep my-report|awk '{print $4}'|awk -F "/" '{print $1}'|sort -ur);do
-	URL="<a href=\"https://mercury.banesco.do/$folder/index.html\">$folder</a>"
+for folder in $(aws s3 ls s3://$BUCKET/ --recursive|grep my-report|awk '{print $4}'|awk -F "/" '{print $1}'|sort -ur);do
+	URL="<a href=\"https://$BUCKET/$folder/index.html\">$folder</a>"
 	echo "<ul>$URL</ul>"  >> index.html
 
 done
